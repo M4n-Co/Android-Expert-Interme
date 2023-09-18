a40f9972a0761f5f66aa1c1dfdb028db81f19521
M4n-Co <M4nprz.H@hotmail.com>
2023-09-18 08:53:03 -0600

    initial commit

---
+++ app/src/main/java/com/example/horoscapp/ui/luck/LuckFragment.kt
@@ -0,0 +1,29 @@
+package com.example.horoscapp.ui.luck
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.horoscapp.R
+import com.example.horoscapp.databinding.FragmentLuckBinding
+
+class LuckFragment : Fragment() {
+
+    private var _binding : FragmentLuckBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLuckBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+}
